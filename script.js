95251c8fd203bd35dc4c7b3c0d7b7d77ca6833cd
Jennifer <jenny.zev@gmail.com>
2021-12-28 11:48:56 +0100

    Modified the calculations

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 
-var billValue;
-var tipValue;
-var peopleNumberValue;
+var billValue; //the bill's value
+var tipValue; //the tip percentage 
+var peopleNumberValue = 0; // the number of people paying
 
 
-const billInput = document.querySelector('#bill-section input');
-var tipButton = document.querySelectorAll('#select-tip div button');
-const tipInput = document.querySelector('#select-tip div input');
-const peopleNumberInput = document.querySelector('#people-section input');
-var tipAmountText = document.querySelector('#result-section #tip-amount span');
-var totalText = document.querySelector('#result-section #total span');
-const resetButton = document.querySelector('#result-section button');
+const billInput = document.querySelector('#bill-section input'); //the input area for the bill value
+var tipButton = document.querySelectorAll('#select-tip div button');// buttons for the tip value
+const tipInput = document.querySelector('#select-tip div input');//custom input area for the tip value
+const peopleNumberInput = document.querySelector('#people-section input'); //input area for the number of people value
+var tipAmountText = document.querySelector('#result-section #tip-amount span');//text which display the tip amount
+var totalText = document.querySelector('#result-section #total span'); //text which display the total amount
+const resetButton = document.querySelector('#result-section button');//reset button
 
-
+//when the the input change the bill value is update and the tip is calculated
 billInput.addEventListener('change',function(){
-    billValue = Number(billInput.value);
+    billValue = Number(billInput.value); //convert the string into a number
     console.log('BillValue:'+ billValue);
 
     tipAmount(billValue, tipValue,peopleNumberValue);
 });
 
+//whenever on of the button is clicked on, the tip percentage is update and the tip calculated. The style changes too.
 for(var i = 0; i < tipButton.length; i++){
 
     tipButton[i].addEventListener("click",function(){
@@ -41,6 +42,7 @@ for(var i = 0; i < tipButton.length; i++){
     })
 }
 
+//whenever the custom tip percentage is changed the tip is calculated and the button disable
 tipInput.addEventListener('change', function(){
     for(var i = 0; i < tipButton.length; i++){ tipButton[i].classList.remove('selected');}
     tipValue = Number(this.value);
@@ -49,12 +51,14 @@ tipInput.addEventListener('change', function(){
     
 })
 
+//if the number of people is changed the tip is calculated
 peopleNumberInput.addEventListener('change',function(){
     peopleNumberValue= Number(peopleNumberInput.value);
     console.log('People Number Value:'+ peopleNumberValue);
     tipAmount(billValue, tipValue,peopleNumberValue);
 })
 
+//reset
 resetButton.addEventListener('click',function(){
     tipAmountText.innerHTML = "0.00";
     totalText.innerHTML = "0.00";
@@ -64,7 +68,7 @@ resetButton.addEventListener('click',function(){
 function tipAmount(billValue, tipValue,peopleNumberValue){
     var TA = billValue*tipValue/100;
     console.log("TA"+TA);
-    if(isNaN(TA) == true){
+    if(isNaN(TA) == true || peopleNumberValue === 0){
     tipAmountText.innerHTML = "0.00";} else{
         tipAmountText.innerHTML = TA;
     }
